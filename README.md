6bab455d3720d0f2622ff899dc4dc7c99c6b55a1
Tomas Delvechio <tdelvechio@unlu.edu.ar>
2020-04-09 17:53:13 -0300

    Adding support to filter by topics. Always add the document for use that

--- README.md
+++ README.md
@@ -3,7 +3,60 @@
 This script simply goes through all the books in this spreadsheet https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/htmlview?urp=gmail_link and downloads them into different folders (that it previously created) according to each book's topic.
 The base directory where the books are downloaded is by default the one where this script is run in. To change it, change the constant `BASE_DIR` to whatever you like, such as `BASE_DIR = /home/books/springer_books` 
 
-### Running the script
+## Running the script
+
 `python ./springer_book_scraper.py`
 
+without arguments, the script download all PDFs of all topics
+
+## Show help
+
+```bash
+python ./springer_book_scraper.py -h
+usage: springer_book_scraper.py [-h] [--topics TOPICS [TOPICS ...]]
+                                [--list-topics]
+
+Download all links from the Google Spreadsheet URL
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --topics TOPICS [TOPICS ...]
+                        List of topics to download
+  --list-topics         Show all available topics
+```
+
+## Filter by topics
+
+This example only downloads of the specified topics:
+
+```bash
+python ./springer_book_scraper.py --topics "Biomedical and Life Sciences" "Medicine" "Computer Science" "Economics and Finance"
+```
+
+**show available topics**
+
+```bash
+python ./springer_book_scraper.py --list-topics
+"Behavioral Science"
+"Behavioral Science and Psychology"
+"Biomedical and Life Sciences"
+"Business and Economics"
+"Business and Management"
+"Chemistry and Materials Science"
+"Computer Science"
+"Earth and Environmental Science"
+"Economics and Finance"
+"Education"
+"Energy"
+"Engineering"
+"Humanities, Social Sciences and Law"
+"Intelligent Technologies and Robotics"
+"Law and Criminology"
+"Literature, Cultural and Media Studies"
+"Mathematics and Statistics"
+"Medicine"
+"Physics and Astronomy"
+"Religion and Philosophy"
+"Social Sciences"
+```
 
--- springer_book_scraper.py
+++ springer_book_scraper.py
@@ -2,10 +2,14 @@ import os
 import requests
 import pandas as pd
 from io import StringIO
+import argparse
+import sys
 
 SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/gviz/tq?tqx=out:csv'
 URL_PDF = 'https://link.springer.com/content/pdf/{codigo_libro}.pdf'
 BASE_DIR = '.'
+
+
 def download_pdfs_from_url(book_url, book_title, book_topic):
     try:
         r = requests.get(book_url)
@@ -18,6 +22,15 @@ def download_pdfs_from_url(book_url, book_title, book_topic):
     except:
         print("Error downloading: {book_topic} - {book_title}".format(book_topic=book_topic, book_title=book_title))
 
+args_parser = argparse.ArgumentParser(description='Download all links from the Google Spreadsheet URL')
+args_parser.add_argument('--topics', nargs='+', help="List of topics to download")
+args_parser.add_argument('--list-topics', action='store_true', help="Show all available topics")
+args = args_parser.parse_args()
+
+if args.list_topics is None and args.topics is None:
+    print("No se indican temas. Esto descargara todos los archivos.\n")
+    print("Si desea filtrar por tema, ejecute ./springer_book_scraper.py -h para obtener ayuda\n")
+
 response = requests.get(SPREADSHEET_URL)
 spreadsheet_csv = StringIO(response.content.decode('utf-8'))
 
@@ -25,10 +38,24 @@ df = pd.read_csv(spreadsheet_csv)
 
 list_of_topics = df['English Package Name'].unique().tolist()
 
-#  creo una carpeta por topico
-for topic in list_of_topics:
+# Handle --list-topics argument
+if args.list_topics:
+    for topic in list_of_topics:
+        print('"{}"'.format(topic))
+    print("\nYou can filter by topics with --topics argument. Example:\n")
+    print('     ./springer_book_scraper.py --topics "Biomedical and Life Sciences" "Medicine"')
+    sys.exit(0)
+
+topics_to_download = list_of_topics
+links_to_download = df
+if args.topics is not None:
+    topics_to_download = args.topics
+    links_to_download = df[df['English Package Name'].isin(topics_to_download)]
+
+#  creo una carpeta por topico valido
+for topic in topics_to_download:
     folder_name = '{base_dir}/{topic}'.format(base_dir=BASE_DIR, topic=topic)
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-df.apply(lambda row: download_pdfs_from_url(row['OpenURL'], row['Book Title'], row['English Package Name']), axis=1)
+links_to_download.apply(lambda row: download_pdfs_from_url(row['OpenURL'], row['Book Title'], row['English Package Name']), axis=1)
